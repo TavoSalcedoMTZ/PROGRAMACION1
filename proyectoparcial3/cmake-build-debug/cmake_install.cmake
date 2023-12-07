82ed37bd18c0e5c963a21eb7253dbeb9500c98ca
Taverx01 <123278391+Taverx01@users.noreply.github.com>
2023-12-06 19:37:49 -0600

    ACTUALIZACION DE CMAKE

--- proyectoparcial3/cmake-build-debug/cmake_install.cmake
+++ proyectoparcial3/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Alumnos/Desktop/Octasalcedo(noborrar)/proyectoparcial3
+# Install script for directory: C:/Users/sdedf/OneDrive/Documentos/programas/proyectoparcial3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Alumnos/Desktop/Octasalcedo(noborrar)/proyectoparcial3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/sdedf/OneDrive/Documentos/programas/proyectoparcial3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
