772679cca2c92da4477283264e9613477bd01010
Taverx01 <123278391+Taverx01@users.noreply.github.com>
2023-12-06 19:38:12 -0600

    ACTUALIZACION DE CMAKE

---
+++ proyectoparcial3/cmake-build-debug/CMakeFiles/proyectoparcial3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyectoparcial3.dir/main.cpp.obj"
+  "CMakeFiles/proyectoparcial3.dir/main.cpp.obj.d"
+  "proyectoparcial3.exe"
+  "proyectoparcial3.exe.manifest"
+  "proyectoparcial3.lib"
+  "proyectoparcial3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyectoparcial3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ proyectoparcial3/cmake-build-debug/CMakeFiles/proyectoparcial3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for proyectoparcial3.
+# This may be replaced when dependencies are built.
